6ce53713f638b1f7ba95127a4f338f58b6dda200
TobiXT <126273238+TobiXT@users.noreply.github.com>
2024-09-27 14:00:06 +0200

    Update Plungin3PreviewRenderer.php

--- Classes/EventListener/PreviewRenderer/Plungin3PreviewRenderer.php
+++ Classes/EventListener/PreviewRenderer/Plungin3PreviewRenderer.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace In2code\Osm\EventListener\PreviewRenderer;
 
-class Plugin1PreviewRenderer extends AbstractPreviewRenderer
+class Plugin3PreviewRenderer extends AbstractPreviewRenderer
 {
     protected string $listType = 'osm_pi3';
 }
